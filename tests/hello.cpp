9ddb833538aa849523c1bb6a2664f24d6d0ae311
Shivashish Das <dasshivashish11@gmail.com>
2025-04-11 00:14:32 +0530

    make test useful

--- tests/hello.cpp
+++ tests/hello.cpp
@@ -3,16 +3,18 @@
 #include <cstdlib>
 
 int main(int argc, const char** argv) {
-    if (argc != 2) {
+    if (argc < 2) {
         std::cout << "USAGE: rv32 {INSTRUCTION}" << std::endl;
         return 1;
     }
 
-    uint32_t n = strtol(argv[1], nullptr, 0);
-	RISCV::DecodedInst di;
-    int status = RISCV::Decode(n, &di);
-	if (status != RISCV::SUCCESS) 
-		return 1;
-	std::cout << "Instruction: " << RISCV::ToString(di) << std::endl;
+	for (int i = 1; i < argc; i++) {
+		uint32_t n = strtol(argv[i], nullptr, 0);
+		RISCV::DecodedInst di;
+		int status = RISCV::Decode(n, &di);
+		if (status != RISCV::SUCCESS) 
+			return 1;
+		std::cout << "Instruction: " << RISCV::ToString(di) << std::endl;
+	}
 	return 0;
 }
