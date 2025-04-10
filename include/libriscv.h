3ee9ba693b43a4f94981b9fb61dd5c9839a379d9
Shivashish Das <dasshivashish11@gmail.com>
2025-04-10 12:58:38 +0530

    add more structure

---
+++ include/libriscv.h
@@ -0,0 +1,9 @@
+#ifndef __LIBRISCV_H__
+#define __LIBRISCV_H__
+
+#if defined(__linux__) || defined(__android__) // Linux-specifc things  
+#endif
+
+#ifdef _MSC_VER // Windows-specific things 
+#endif 
+#endif
--- src/lib.cpp
+++ src/lib.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <libriscv.h>
 
 int support() {
     return 0;
